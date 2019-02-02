5b59383757422832d36f001dd4be7c006ff43340
ExploHash <jbtwaalf@gmail.com>
2019-02-02 22:58:05 +0100

    Added validation against XSS, shorted error messages code and callback for db operation

--- routes/api.js
+++ routes/api.js
@@ -12,26 +12,30 @@ router.post('/register', function(req, res, next) {
     for(let parmIndex in requiredParms){
         let parm = requiredParms[parmIndex];
         if(req.body[parm] === undefined || req.body[parm].length < 1){
-            res.status(400).json({message: "Missing " + parm, code: "InvalidParameterException"});
+            valError(res, "Mising " + parm);
             return next();
         }
         //check if string
         if(typeof parm !== "string"){
-            res.status(400).json({message: parm + " is not a string", code: "InvalidParameterException"});
+            valError(res, parm + " is not a string");
             return next();
         }
     }
     //check meer
     if(req.body.name.length > 100){
-        res.status(400).json({message: req.body.name + " can't be longer than 100 chars", code: "InvalidParameterException"});
+        valError(res, req.body.name + " can't be longer than 100 chars");
+    }else if(!validateName(req.body.name)){
+        valError(res, "Name can only consist of numbers and letters");
     }else if(req.body.surname.length > 100){
-        res.status(400).json({message: req.body.surname + " can't be longer than 100 chars", code: "InvalidParameterException"});
+        valError(res, req.body.surname + " can't be longer than 100 chars");
+    }else if(!validateName(req.body.surname)){
+        valError(res, "Surname can only consist of numbers and letters");
     }else if(!validateEmail(req.body.email)){
-        res.status(400).json({message: req.body.email + " is not real", code: "InvalidParameterException"});
+        valError(res, req.body.email + " is not a real email adress");
     }else if(req.body.password > 200){
-        res.status(400).json({message: req.body.password + " can't be longer than 200 chars", code: "InvalidParameterException"});
+        valError(res, req.body.password + " can't be longer than 200 chars");
     }else if(req.body.password !== req.body.passwordRepeat){
-        res.status(400).json({message: "Passwords are not equal!", code: "InvalidParameterException"});
+        valError(res, "Passwords are not equal!");
     }else{
         //check of er al een email bestaat
         db.find({email: req.body.email}, function(err, results){
@@ -44,10 +48,15 @@ router.post('/register', function(req, res, next) {
                     surname: req.body.surname,
                     password: crypto.createHash("sha512").update(req.body.password).digest('hex'),
                 }
-                db.insert(object);
-                res.redirect("/");
+                db.insert(object, function(err, inserted){
+                    if(err){
+                        res.status(500).json({message: "Failed to write to database"});
+                    }else{
+                        res.redirect("/");
+                    }
+                });
             }else{
-                res.status(400).json({message: "Email already exists"});
+                valError(res, "Email already exists");
             }
         });
     }
@@ -62,7 +71,7 @@ router.post('/login', function(req, res, next) {
         //find een gebruiker met hetzelfde gebruikersnaam en wachtwoord en log hem daarna in
         db.find(search, function(err, results){
             if(err || results.length === 0){
-                res.status(400).json({message: "Email or password incorrect!"});
+                valError(res, "Email or password incorrect!");
             }else{
                 req.session.isLoggedIn = true;
                 req.session.name = results[0].name + " " + results[0].surname;
@@ -70,7 +79,7 @@ router.post('/login', function(req, res, next) {
             }
         });
     }else{
-        res.json({message: "Please provide email and password"});
+        valError(res, "Please provide email and password");
     }
 });
 //verkrijg de loginstatys
@@ -90,6 +99,15 @@ router.get('/logout', function(req, res, next){
 function validateEmail(email) {//opensource functie gebruik door chrome
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+}
+
+function validateName(name){
+    var re = /^[a-zA-Z0-9_.-]*$/;
+    return re.test(String(name));
+}
+
+function valError(res, message){
+    res.status(400).json({message: message, code: "InvalidParameterException"});
 }
 
 module.exports = router;
