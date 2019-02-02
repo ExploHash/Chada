e127c747d92b70a6d423f6d9624c9b7fb484acb8
ExploHash <jbtwaalf@gmail.com>
2019-02-02 22:56:59 +0100

    Add welcome message and implemented error system by disconnects

--- public/JS/chat.js
+++ public/JS/chat.js
@@ -1,7 +1,7 @@
-var socket = io(); //websockets library
-
 var chat = {
-    init: function(){
+    init(){
+        //initialize socket
+        this.socket = io(); //websockets library
         document.getElementById("text").addEventListener("keydown", function(e){//als iemand op enter drukt
             if(e.keyCode === 13){
                 chat.sendMessage(e.target);
@@ -10,22 +10,38 @@ var chat = {
         chat.initializeSocket();
     },
     initializeSocket(){//start de event lsiteners
-        socket.on('message', function(msg){
-            console.log("Received", msg);
-            chat.pushMessage(msg);
+        this.socket.on("connect", function(){
+            console.log("Connected!");
+            chat.pushMessage("Welkom. Je bent verbonden met de chatserver.");
+            //reset reconnectTimes
+            if(parseInt(localStorage.getItem("reconnectTimes")) > 0){
+                localStorage.setItem("reconnectTimes", 0);
+            }
         });
-        
-        socket.on('disconnect', function(){
-            chat.pushMessage("Verbinding verloren. Mogelijk is je sessie verlopen, refresh de pagina. Of je hebt geen internet meer.")
+        this.socket.on('message', function(msg){//wanneer een bericht binen komt
+            chat.pushMessage(msg);
+        });        
+        this.socket.on('disconnect', function(){//Wanneer een socket disconnect
+            var times = parseInt(localStorage.getItem("reconnectTimes") || 0); //verkrijg reconnect telling van cookie
+            if(times < 3){//minder dan 3
+                localStorage.setItem("reconnectTimes", ++times);
+                chat.pushMessage("Connectie gefaald. Opnieuw proberen...");
+                setTimeout(function(){//herlaad
+                    window.location.reload();
+                }, 1000);
+            }else{
+                localStorage.setItem("reconnectTimes", 0);
+                chat.pushMessage("Mislukt. Mogelijk ben je uitgelogd of is de verbinding verloren. Refresh de pagina");
+            }
         });
     },
     sendMessage(el){//verzend een bericht naar de rest
         var message = "Jij: " + el.value;
         chat.pushMessage(message);
-        socket.emit('message', el.value); //naar server
+        this.socket.emit('message', el.value); //naar server
         el.value = "";
     },
-    pushMessage: function(message){//laat een bericht zien in de textarea
+    pushMessage(message){//laat een bericht zien in de textarea
         var textarea = document.getElementById("texta");
         textarea.innerHTML += message + "<br>";
         textarea.scroll(0,textarea.scrollHeight);
